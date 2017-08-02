ea2695b9ebf4fe067efff46d5509938d21349716
elbosso <elbosso@tichy.fritz.box>
2017-08-02 07:51:50 +0200

    started working on #5516 (In component ca importcert: DI für tsa erzeugen (automatisch?))

--- create_timestamp.sh
+++ create_timestamp.sh
@@ -63,6 +63,88 @@ fi
 ca_dir_name=`realpath .`
 # Noch keine DI für TSA existiert
 if [ ! -e "./ca/private/tsa.key" ]; then
+proposed_pass=`makepasswd -count 1 -minchars 8`
+condition=1
+while [ $condition -eq 1 ]
+do
+condition=0
+result=$($dialog_exe --stdout --backtitle "Password for private key of timestamp authority" \
+	    --form " Please specify - use [up] [down] to select input field " 0 0 0 \
+	    "Password" 2 4 "$proposed_pass" 2 25 40 0\
+	    "Verification" 4 4 "" 4 25 40 0)
+
+	if [ ${?} -ne 0 ]; then exit 127; fi
+#    result=`cat $_temp`
+    echo "Result=$result"
+
+Password=`echo "$result"|cut -d"
+" -f 1`
+Verification=`echo "$result" |cut -d"
+" -f 2`
+#$dialog_exe --backtitle "Info" --msgbox "$result\n--\n$Password\n$Verification" 9 52
+if [ "$Password" = "" ] || [ "$Verification" = "" ]; then
+echo "You must fill out all the fields!"
+$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields!" 9 52
+condition=1
+fi
+if [ "$Verification" != "$Password" ] && [ $condition -eq 0 ]; then
+echo "Password and verification differ!"
+$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields identically!" 9 52
+condition=1
+fi
+if [ ${#Password} -lt 4 ] && [ $condition -eq 0 ]; then
+$dialog_exe --backtitle "Error" --msgbox "A password must be at least 4 characters long!" 9 52
+condition=1
+fi
+done
+proposed_pass=""
+
+condition=1
+while [ $condition -eq 1 ]
+do
+condition=0
+countryName=$(grep countryName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+stateOrProvinceName=$(grep stateOrProvinceName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+localityName=$(grep localityName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+organizationName=$(grep organizationName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+organizationalUnitName=$(grep organizationalUnitName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+commonName=$(grep commonName_default "./etc/timestamp.conf"|cut -d " " -f 3|cut -d "\"" -f 2)
+$dialog_exe --backtitle "CSR defaults for $item" \
+	    --form " Specify defaults for $item - use [up] [down] to select input field " 0 0 0 \
+	    "countryName" 2 4 "$countryName" 2 25 40 0\
+	    "stateOrProvinceName" 4 4 "$stateOrProvinceName" 4 25 40 0\
+	    "localityName" 6 4 "$localityName" 6 25 40 0\
+	    "organizationName" 8 4 "$organizationName" 8 25 40 0\
+	    "organizationalUnitName" 10 4 "$organizationalUnitName" 10 25 40 0\
+	    "commonName" 12 4 "$commonName" 12 25 64 0\
+	    2>$_temp
+
+	if [ ${?} -ne 0 ]; then exit 127; fi
+    result=`cat $_temp`
+    echo "Result=$result"
+countryName=`cat $_temp |cut -d"
+" -f 1`
+stateOrProvinceName=`cat $_temp |cut -d"
+" -f 2`
+localityName=`cat $_temp |cut -d"
+" -f 3`
+organizationName=`cat $_temp |cut -d"
+" -f 4`
+organizationalUnitName=`cat $_temp |cut -d"
+" -f 5`
+commonName=`cat $_temp |cut -d"
+" -f 6`
+if [ "$countryName" = "" ] || [ "$stateOrProvinceName" = "" ] || [ "$localityName" = "" ] || [ "$organizationName" = "" ] || [ "$organizationalUnitName" = "" ] || [ "$commonName" = "" ] ; then
+echo "You must fill out all the fields!"
+$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields!" 9 52
+condition=1
+fi
+done
+
+
+
+
+
 #der private Schlüssel wird ausgewählt
 if [ "$privkey_file_name" == "" ]; then
 	privkey_file_name=$($dialog_exe --stdout --backtitle "CAs Private Key" --fselect "$ca_dir_name/ca/private/" 0 90)
