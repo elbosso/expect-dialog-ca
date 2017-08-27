48e3814a00255288f73d8b30bf8e05157027a091
elbosso <elbosso@tichy.fritz.box>
2017-08-27 19:53:05 +0200

    re #5538

--- create_timestamp.sh
+++ create_timestamp.sh
@@ -63,41 +63,9 @@ fi
 ca_dir_name=`realpath .`
 # Noch keine DI für TSA existiert
 if [ ! -e "./ca/private/tsa.key" ]; then
-proposed_pass=`makepasswd -count 1 -minchars 8`
-condition=1
-while [ $condition -eq 1 ]
-do
-condition=0
-result=$($dialog_exe --stdout --backtitle "Password for private key of timestamp authority" \
-	    --form " Please specify - use [up] [down] to select input field " 0 0 0 \
-	    "Password" 2 4 "$proposed_pass" 2 25 40 0\
-	    "Verification" 4 4 "" 4 25 40 0)
-
-	if [ ${?} -ne 0 ]; then exit 127; fi
-#    result=`cat $_temp`
-    echo "Result=$result"
-
-Password=`echo "$result"|cut -d"
-" -f 1`
-Verification=`echo "$result" |cut -d"
-" -f 2`
-#$dialog_exe --backtitle "Info" --msgbox "$result\n--\n$Password\n$Verification" 9 52
-if [ "$Password" = "" ] || [ "$Verification" = "" ]; then
-echo "You must fill out all the fields!"
-$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields!" 9 52
-condition=1
-fi
-if [ "$Verification" != "$Password" ] && [ $condition -eq 0 ]; then
-echo "Password and verification differ!"
-$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields identically!" 9 52
-condition=1
-fi
-if [ ${#Password} -lt 4 ] && [ $condition -eq 0 ]; then
-$dialog_exe --backtitle "Error" --msgbox "A password must be at least 4 characters long!" 9 52
-condition=1
-fi
-done
-proposed_pass=""
+purpose=" of timestamp authority"
+. ../ask_for_passwd.sh
+purpose=""
 
 condition=1
 while [ $condition -eq 1 ]
@@ -174,41 +142,7 @@ condition=1
 fi
 done
 
-proposed_pass=`makepasswd -count 1 -minchars 8`
-condition=1
-while [ $condition -eq 1 ]
-do
-condition=0
-result=$($dialog_exe --stdout --backtitle "Password for private key" \
-	    --form " Please specify - use [up] [down] to select input field " 0 0 0 \
-	    "Password" 2 4 "$proposed_pass" 2 25 40 0\
-	    "Verification" 4 4 "" 4 25 40 0)
-
-	if [ ${?} -ne 0 ]; then exit 127; fi
-#    result=`cat $_temp`
-    echo "Result=$result"
-
-Password=`echo "$result"|cut -d"
-" -f 1`
-Verification=`echo "$result" |cut -d"
-" -f 2`
-#$dialog_exe --backtitle "Info" --msgbox "$result\n--\n$Password\n$Verification" 9 52
-if [ "$Password" = "" ] || [ "$Verification" = "" ]; then
-echo "You must fill out all the fields!"
-$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields!" 9 52
-condition=1
-fi
-if [ "$Verification" != "$Password" ] && [ $condition -eq 0 ]; then
-echo "Password and verification differ!"
-$dialog_exe --backtitle "Error" --msgbox "You must fill out all the fields identically!" 9 52
-condition=1
-fi
-if [ ${#Password} -lt 4 ] && [ $condition -eq 0 ]; then
-$dialog_exe --backtitle "Error" --msgbox "A password must be at least 4 characters long!" 9 52
-condition=1
-fi
-done
-proposed_pass=""
+. ./ask_for_passwd.sh
 
 mv ${privkey_file_name} ${privkey_file_name}.bck
 
