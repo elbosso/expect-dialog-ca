8012952b55783a831a6c38255bbed2a2728a8baf
Jürgen Key <sgjfsf@hotmail.com>
2019-10-10 18:48:32 +0200

    fixes #9

--- build_p12.sh
+++ build_p12.sh
@@ -8,13 +8,17 @@ if [ ! -e ./tmp ];then
 	mkdir ./tmp
 fi
 cns=`grep Subject: issuer.crt |sed -E "s/(.*?)CN=(.*?)/\2/g"`
+echo "$cns"
 . ../index.txt
-scn=`grep Subject: ../$zert |sed -E "s/(.*?)CN=(.*?)/\2/g"`
+echo "$zert"
+scn=$(grep Subject: "../$zert" |sed -E "s/(.*?)CN=(.*?)/\2/g")
+echo "$scn"
 stmta="openssl pkcs12 -export -name \"$scn\" "
 for item in ${cns}
     do
 		stmta="$stmta -caname \"$item\" "
 	done
-stmta="${stmta} -inkey $1 -in ../$zert -certfile issuer.crt -out ${scn}.p12"
-`$stmta`
+stmta="${stmta} -inkey \"$1\" -in \"../$zert\" -certfile issuer.crt -out \"${scn}.p12\""
+echo "$stmta"
+eval $stmta
 echo "P12 file \"`readlink -f ${scn}.p12`\" created"
