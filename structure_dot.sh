4580ef2b215993c0523ce41cdcd9a0311f2777ef
Jürgen Key <sgjfsf@hotmail.com>
2023-10-21 15:03:42 +0200

    added lifetime to grqaphviz representation

--- structure_dot.sh
+++ structure_dot.sh
@@ -17,9 +17,10 @@ do
     sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
     sdnu=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p'| tr '[:lower:]' '[:upper:]')
     if [[ $sdnu == *"TEST"* && ! $sdnu == *"INDEPENDENT"* ]]; then
-      #enddate=$(openssl x509 -noout -enddate -in "${cert}"|cut -d '=' -f 2)
+      startdate=$(openssl x509 -noout -startdate -in "${cert}"|cut -d '=' -f 2)
+      enddate=$(openssl x509 -noout -enddate -in "${cert}"|cut -d '=' -f 2)
       #echo "${subject} [shape=box label=\"$sdn\lexpires on: $enddate\"]">>graphviz.dot
-      echo "${subject} [shape=box label=\"$sdn\"]">>graphviz.dot
+      echo "${subject} [shape=box label=<$sdn<br align=\"left\"/><font point-size=\"8\">From: $startdate<br/>To: $enddate</font>>]">>graphviz.dot
     fi
   fi
 done
@@ -34,7 +35,7 @@ do
     sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
     sdnu=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p'| tr '[:lower:]' '[:upper:]')
     if [[ $sdnu == *"TEST"* && $sdnu == *"INDEPENDENT"* ]]; then
-      echo "${subject} [shape=box label=\"$sdn\"]">>graphviz.dot
+      echo "${subject} [shape=box label=<$sdn<br align=\"left\"/><font point-size=\"8\">From: $startdate<br/>To: $enddate</font>>]">>graphviz.dot
     fi
   fi
 done
@@ -52,7 +53,7 @@ do
     sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
     sdnu=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p'| tr '[:lower:]' '[:upper:]')
     if [[ $sdnu == *"EXT PARTY"* || $sdnu == *"EXTERNAL PARTY"* ]]; then
-      echo "${subject} [shape=box label=\"$sdn\"]">>graphviz.dot
+      echo "${subject} [shape=box label=<$sdn<br align=\"left\"/><font point-size=\"8\">From: $startdate<br/>To: $enddate</font>>]">>graphviz.dot
     fi
   fi
 done
@@ -68,7 +69,7 @@ do
     sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
     sdnu=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p'| tr '[:lower:]' '[:upper:]')
     if [[ ! $sdnu == *"EXTERNAL PARTY"* && ! $sdnu == *"EXT PARTY"* && ! $sdnu == *"TEST"* ]]; then
-      echo "${subject} [shape=box label=\"$sdn\"]">>graphviz.dot
+      echo "${subject} [shape=box label=<$sdn<br align=\"left\"/><font point-size=\"8\">From: $startdate<br/>To: $enddate</font>>]">>graphviz.dot
     fi
   fi
 done
@@ -81,7 +82,7 @@ do
 #    echo "found: $cert"
     subject=$(openssl x509 -noout -subject -in "${cert}"|cut -d '=' -f 2-| sed 's/[\*\.\" ,=-]//g')
     issuer=$(openssl x509 -noout -issuer -in "${cert}"|cut -d '=' -f 2-| sed 's/[\*\.\" ,=-]//g')
-    sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
+    #sdn=$(openssl x509 -noout -subject -in "${cert}"| sed -n '/^subject/s/^.*CN\s*=\s*//p')
     echo "$issuer -> ${subject}">>graphviz.dot
   fi
 
